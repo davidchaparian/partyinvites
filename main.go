e1c6c80ade9bdb89dde9be84e1f794b2dde67799
davidchaparian <d3723518@gmail.com>
2024-08-03 13:23:45 +0300

    add post method & complete fuctionality

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ type formData struct {
 	Errors []string
 }
 
-var resonses = make([]*Rsvp, 0, 10)
+var responses = make([]*Rsvp, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
 func loadTempletes() {
@@ -36,7 +36,7 @@ func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func listHandler(writer http.ResponseWriter, request *http.Request) {
-	templates["list"].Execute(writer, resonses)
+	templates["list"].Execute(writer, responses)
 }
 
 func formHandler(writer http.ResponseWriter, request *http.Request) {
@@ -44,6 +44,22 @@ func formHandler(writer http.ResponseWriter, request *http.Request) {
 		templates["form"].Execute(writer, formData{
 			Rsvp: &Rsvp{}, Errors: []string{},
 		})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willattend"][0] == "true",
+		}
+
+		responses = append(responses, &responseData)
+
+		if responseData.WillAttend {
+			templates["thanks"].Execute(writer, responseData.Name)
+		} else {
+			templates["sorry"].Execute(writer, responseData.Name)
+		}
 	}
 }
 
